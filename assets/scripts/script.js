9c543786f89701b3e07525e5744b075ce676837d
DaniloCorreia1 <101065063+DaniloCorreia1@users.noreply.github.com>
2024-09-03 19:55:58 -0300

    Upload do site de portifolio

---
+++ assets/scripts/script.js
@@ -0,0 +1,32 @@
+function closeAllDescriptions() {
+    var descriptions = document.querySelectorAll('.description');
+    descriptions.forEach(function(description) {
+        description.style.display = 'none';
+    });
+}
+
+function toggleDescription(id) {
+    var description = document.getElementById(id);
+    if (description.style.display === 'none' || description.style.display === '') {
+        closeAllDescriptions();
+        description.style.display = 'block';
+    } else {
+        description.style.display = 'none';
+    }
+}
+
+document.getElementById('toggleItem1').addEventListener('click', function() {
+    toggleDescription('description1');
+});
+
+document.getElementById('toggleItem2').addEventListener('click', function() {
+    toggleDescription('description2');
+});
+
+document.getElementById('toggleItem3').addEventListener('click', function() {
+    toggleDescription('description3');
+});
+
+document.getElementById('toggleItem4').addEventListener('click', function() {
+    toggleDescription('description4');
+});
